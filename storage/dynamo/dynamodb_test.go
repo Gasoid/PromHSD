ce96e95ce44578003bd63082c5b853e7a2a2550c
Gasoid <gasoid@gmail.com>
2023-01-08 14:24:41 +0100

    more tests

--- storage/dynamo/dynamodb_test.go
+++ storage/dynamo/dynamodb_test.go
@@ -37,6 +37,14 @@ func (item *testDeleteItem) DeleteItem(*dynamodb.DeleteItemInput) (*dynamodb.Del
 	return nil, item.err
 }
 
+type testDescribeTable struct {
+	err error
+}
+
+func (item *testDescribeTable) DescribeTable(*dynamodb.DescribeTableInput) (*dynamodb.DescribeTableOutput, error) {
+	return nil, item.err
+}
+
 func TestDynamoDB_Get(t *testing.T) {
 	type fields struct {
 		IGetItem  IGetItem
@@ -140,6 +148,77 @@ func TestDynamoDB_Delete(t *testing.T) {
 			if err := d.Delete(tt.args.target); (err != nil) != tt.wantErr {
 				assert.Error(t, err)
 			}
+		})
+	}
+}
+
+func TestDynamoDB_Create(t *testing.T) {
+	target := db.NewTarget()
+	target.ID = db.ID("id")
+	target.Name = "example"
+	type fields struct {
+		IPutItem  IPutItem
+		IGetItem  IGetItem
+		tableName string
+	}
+	type args struct {
+		target *db.Target
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "NotFoundError",
+			fields: fields{
+				IPutItem: &testPutItem{err: awserr.New(dynamodb.ErrCodeResourceNotFoundException, "notFound", nil)},
+				IGetItem: &testPutItem{err: awserr.New(dynamodb.ErrCodeResourceNotFoundException, "notFound", nil)},
+			},
+			args:    args{target: target},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			d := &DynamoDB{
+				IPutItem:  tt.fields.IPutItem,
+				IGetItem:  tt.fields.IGetItem,
+				tableName: tt.fields.tableName,
+			}
+			if err := d.Create(tt.args.target); (err != nil) != tt.wantErr {
+				assert.Error(t, err)
+			}
+		})
+	}
+}
+
+func TestDynamoDB_IsHealthy(t *testing.T) {
+	type fields struct {
+		IDescribeTable IDescribeTable
+		tableName      string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{
+			name: "NotFoundError",
+			fields: fields{
+				IDescribeTable: &testDescribeTable{err: awserr.New(dynamodb.ErrCodeResourceNotFoundException, "notFound", nil)},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			d := &DynamoDB{
+				IDescribeTable: tt.fields.IDescribeTable,
+				tableName:      tt.fields.tableName,
+			}
+			assert.Equal(t, tt.want, d.IsHealthy())
 		})
 	}
 }
